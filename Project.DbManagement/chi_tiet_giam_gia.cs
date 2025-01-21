523353bb69ebbfe0a67aaf3e72d1a14d9f063b32
cococailon <12sonkieu12@gmail.com>
2025-01-21 22:53:19 +0700

    Thêm các bảng: giam_gia, chi_tiet_giam_gia, hoa_don, phuong_thuc_thanh_toan, hoa_don_chi_tiet, thong_tin_lien_he. Và thêm DbSet vào DbContext.

---
+++ Project.DbManagement/chi_tiet_giam_gia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DbManagement
+{
+    public class chi_tiet_giam_gia
+    {
+        public Guid id_giam_gia { get; set; }
+        public Guid id_hoa_don { get; set; }
+        public DateTime create_on_date { get; set; }
+        public DateTime last_modifi_on_date { get; set; }
+
+        [ForeignKey("id_giam_gia")]
+        public virtual giam_gia Giam_Gia { get; set; }
+
+        [ForeignKey("id_hoa_don")]
+        public virtual hoa_don Hoa_Don { get; set; }
+    }
+}
---
+++ Project.DbManagement/thong_tin_lien_he.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DbManagement
+{
+    public class thong_tin_lien_he
+    {
+        [Key]
+        public Guid id { get; set; }
+        public string sdt { get; set; }
+        public string ho { get; set; }
+        public string ten_dem { get; set; }
+    }
+}
