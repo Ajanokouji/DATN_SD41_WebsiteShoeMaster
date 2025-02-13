d4d9a0efcbb2a68c0d0452de4a9dd974b9266566
PhanHuy280302Bg <122019288+PhanHuy280302Bg@users.noreply.github.com>
2025-02-13 22:07:08 +0700

    user

---
+++ Project.Business/Model/UserQueryModel.cs
@@ -0,0 +1,23 @@
+﻿using Project.Common;
+using SERP.Metadata.Models.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Business.Model
+{
+   public class UserQueryModel : BaseRequestModel, IListMetadataFilterQuery
+    {
+        public string? Type { get; set; }
+        public string? Username { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+        public string? AvartarUrl { get; set; }
+        public string? Password { get; set; }
+        public string? UserDetailJson { get; set; }
+    }
+}
