6c07aa7db6e1f503bdcf3ab245ec73ccb28df7ed
QuynbDev2k3 <quynbdev@gmail.com>
2025-02-15 00:08:37 +0700

    business_billdetais

--- Project.Business/Implement/BillDetailsBusiness.cs
+++ Project.Business/Implement/BillDetailsBusiness.cs
@@ -7,177 +7,99 @@ using SERP.Framework.Common;
 
 namespace Project.Business.Implement;
 
-public class BillBusiness : IBillBusiness
+public class BillDetailsBusiness : IBillDetailsBusiness
 {
-    private readonly IBillRepository _billRepository;
+    private readonly IBillDetailsRepository _iBillDetailsRepository;
 
-    public BillBusiness(IBillRepository billRepository)
+    public BillDetailsBusiness(IBillDetailsRepository iBillDetailsRepository)
     {
-        _billRepository = billRepository;
+        _iBillDetailsRepository = iBillDetailsRepository;
     }
 
-    public async Task<BillEntity> DeleteAsync(Guid contentId)
+    public async Task<BillDetails> DeleteAsync(Guid contentId)
     {
-        return await _billRepository.DeleteAsync(contentId);
+        return await _iBillDetailsRepository.DeleteAsync(contentId);
     }
 
-    public async Task<IEnumerable<BillEntity>> DeleteAsync(Guid[] deleteIds)
+    public async Task<IEnumerable<BillDetails>> DeleteAsync(Guid[] deleteIds)
     {
-        return await _billRepository.DeleteAsync(deleteIds);
+        return await _iBillDetailsRepository.DeleteAsync(deleteIds);
     }
 
-    public async Task<BillEntity> FindAsync(Guid contentId)
+    public async Task<BillDetails> FindAsync(Guid contentId)
     {
-        return await _billRepository.FindAsync(contentId);
+        return await _iBillDetailsRepository.FindAsync(contentId);
     }
 
-    public async Task<Pagination<BillEntity>> GetAllAsync(BillQueryModel queryModel)
+    public async Task<Pagination<BillDetails>> GetAllAsync(BillDetailsQueryModel queryModel)
     {
-        return await _billRepository.GetAllAsync(queryModel);
+        return await _iBillDetailsRepository.GetAllAsync(queryModel);
     }
 
-    public async Task<int> GetCountAsync(BillQueryModel queryModel)
+    public async Task<int> GetCountAsync(BillDetailsQueryModel queryModel)
     {
-        return await _billRepository.GetCountAsync(queryModel);
+        return await _iBillDetailsRepository.GetCountAsync(queryModel);
     }
 
-    public async Task<IEnumerable<BillEntity>> ListAllAsync(BillQueryModel queryModel)
+    public async Task<IEnumerable<BillDetails>> ListAllAsync(BillDetailsQueryModel queryModel)
     {
-        return await _billRepository.ListAllAsync(queryModel);
+        return await _iBillDetailsRepository.ListAllAsync(queryModel);
     }
 
-    public async Task<IEnumerable<BillEntity>> ListByIdsAsync(IEnumerable<Guid> ids)
+    public async Task<IEnumerable<BillDetails>> ListByIdsAsync(IEnumerable<Guid> ids)
     {
-        return await _billRepository.ListByIdsAsync(ids);
+        return await _iBillDetailsRepository.ListByIdsAsync(ids);
     }
 
-    public async Task<BillEntity> PatchAsync(BillEntity model)
+    public async Task<BillDetails> PatchAsync(BillDetails model)
     {
-        var exist = await _billRepository.FindAsync(model.id_hoa_don);
+        var exist = await _iBillDetailsRepository.FindAsync(model.id_hoa_don_chi_tiet);
 
         if (exist == null)
         {
             throw new ArgumentException(BillConstant.BillNotFound);
         }
 
-        var update = new BillEntity()
+        var update = new BillDetails()
         {
+            id_hoa_don_chi_tiet = exist.id_hoa_don_chi_tiet,
             id_hoa_don = exist.id_hoa_don,
-            id_nhan_vien = exist.id_nhan_vien,
-            id_khach_hang = exist.id_khach_hang,
-            id_don_hang = exist.id_don_hang,
-            id_phuong_thuc_thanh_toan = exist.id_phuong_thuc_thanh_toan,
-            ma_hoa_don = exist.ma_hoa_don,
-            ten_khach_nhan = exist.ten_khach_nhan,
-            so_dien_thoai_khach_nhan = exist.so_dien_thoai_khach_nhan,
-            dia_chi_nhan = exist.dia_chi_nhan,
-            tong_tien = exist.tong_tien,
-            tong_tien_khuyen_mai = exist.tong_tien_khuyen_mai,
-            tong_tien_sau_khuyen_mai = exist.tong_tien_sau_khuyen_mai,
-            tong_tien_phai_thanh_toan = exist.tong_tien_phai_thanh_toan,
+            id_san_pham_chi_tiet = exist.id_san_pham_chi_tiet,
+            ma_hoa_don_chi_tiet = exist.ma_hoa_don_chi_tiet,
             trang_thai = exist.trang_thai,
-            trang_thai_thanh_toan = exist.trang_thai_thanh_toan,
-            create_on_date = exist.create_on_date,
-            email_khach_nhan = exist.email_khach_nhan,
-            last_modifi_on_date = exist.last_modifi_on_date,
-            update_by = exist.update_by,
-            ghi_chu = exist.ghi_chu,
-            LastModifiedByUserId = exist.LastModifiedByUserId
+            so_luong = exist.so_luong,
+            don_gia = exist.don_gia,
+            ghi_chu = exist.ghi_chu
         };
 
-        if (!string.IsNullOrWhiteSpace(model.ma_hoa_don))
+        if (!string.IsNullOrWhiteSpace(model.ma_hoa_don_chi_tiet))
         {
-            update.ma_hoa_don = model.ma_hoa_don;
+            update.ma_hoa_don_chi_tiet = model.ma_hoa_don_chi_tiet;
         }
 
-        if (model.id_nhan_vien != null)
+        if (model.id_hoa_don != null)
         {
-            update.id_nhan_vien = model.id_nhan_vien;
+            update.id_hoa_don = model.id_hoa_don;
         }
         
-        if (model.id_khach_hang != null)
+        if (model.id_san_pham_chi_tiet != null)
         {
-            update.id_nhan_vien = model.id_nhan_vien;
+            update.id_san_pham_chi_tiet = model.id_san_pham_chi_tiet;
         }
         
-        if (model.id_don_hang != null)
-        {
-            update.id_nhan_vien = model.id_nhan_vien;
-        }
-        
-        if (model.id_phuong_thuc_thanh_toan != null)
-        {
-            update.id_nhan_vien = model.id_nhan_vien;
-        }
-
-        if (!string.IsNullOrWhiteSpace(model.ten_khach_nhan))
-        {
-            update.ten_khach_nhan = model.ten_khach_nhan;
-        }
-
-        if (!string.IsNullOrWhiteSpace(model.so_dien_thoai_khach_nhan))
-        {
-            update.so_dien_thoai_khach_nhan = model.so_dien_thoai_khach_nhan;
-        }
-
-        if (!string.IsNullOrWhiteSpace(model.email_khach_nhan))
-        {
-            update.email_khach_nhan = model.email_khach_nhan;
-        }
-
-        if (!string.IsNullOrWhiteSpace(model.dia_chi_nhan))
-        {
-            update.dia_chi_nhan = model.dia_chi_nhan;
-        }
-
-        if (model.tong_tien > 0)
-        {
-            update.tong_tien = model.tong_tien;
-        }
-
-        if (model.tong_tien_khuyen_mai > 0)
-        {
-            update.tong_tien_khuyen_mai = model.tong_tien_khuyen_mai;
-        }  
-
-        if (model.tong_tien_sau_khuyen_mai > 0)
-        {
-            update.tong_tien_sau_khuyen_mai = model.tong_tien_sau_khuyen_mai;
-        }
-
-        if (model.tong_tien_sau_khuyen_mai > 0)
-        {
-            update.tong_tien_sau_khuyen_mai = model.tong_tien_sau_khuyen_mai;
-        }
-        
-        if (model.tong_tien_phai_thanh_toan > 0)
-        {
-            update.tong_tien_phai_thanh_toan = model.tong_tien_phai_thanh_toan;
-        }
-
         if (model.trang_thai > 0)
         {
             update.trang_thai = model.trang_thai;
         }
 
-        if (model.trang_thai_thanh_toan > 0)
+        if (model.so_luong > 0)
         {
-            update.trang_thai_thanh_toan = model.trang_thai_thanh_toan;
+            update.so_luong = model.so_luong;
         }
-
-        if (model.create_on_date != null)
+        
+        if (model.don_gia > 0)
         {
-            update.create_on_date = model.create_on_date;
-        }
-
-        if (model.last_modifi_on_date != null)
-        {
-            update.last_modifi_on_date = model.last_modifi_on_date;
-        }
-
-        if (!string.IsNullOrWhiteSpace(model.update_by))
-        {
-            update.update_by = model.update_by;
+            update.don_gia = model.don_gia;
         }
 
         if (!string.IsNullOrWhiteSpace(model.ghi_chu))
@@ -188,12 +110,12 @@ public class BillBusiness : IBillBusiness
         return await SaveAsync(update);
     }
 
-    public async Task<BillEntity> SaveAsync(BillEntity billEntity)
+    public async Task<BillDetails> SaveAsync(BillDetails billDetails)
     {
-        return await SaveAsync(new[] { billEntity });
+        return await SaveAsync(new[] { billDetails });
     }
 
-    public Task<BillEntity> SaveAsync(IEnumerable<BillEntity> billEntities)
+    public Task<BillDetails> SaveAsync(IEnumerable<BillDetails> billDetailsEntities)
     {
         throw new NotImplementedException();
     }
