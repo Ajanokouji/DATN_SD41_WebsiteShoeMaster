0b72c2237d7960c3e3b40c5d38091f80d53e049b
QuynbDev2k3 <quynbdev@gmail.com>
2025-02-15 00:05:17 +0700

    business_bill

---
+++ Project.Business/Interface/IBillBusiness.cs
@@ -0,0 +1,28 @@
+﻿using Project.Business.Model;
+using Project.DbManagement;
+using SERP.Framework.Common;
+
+namespace Project.Business.Interface;
+
+public interface IBillBusiness
+{
+    Task<Pagination<BillEntity>> GetAllAsync(BillQueryModel queryModel);
+
+    Task<IEnumerable<BillEntity>> ListAllAsync(BillQueryModel queryModel);
+
+    Task<int> GetCountAsync(BillQueryModel queryModel);
+
+    Task<IEnumerable<BillEntity>> ListByIdsAsync(IEnumerable<Guid> ids);
+
+    Task<BillEntity> FindAsync(Guid contentId);
+
+    Task<BillEntity> DeleteAsync(Guid contentId);
+    
+    Task<IEnumerable<BillEntity>> DeleteAsync(Guid[] deleteIds);
+
+    Task<BillEntity> SaveAsync(BillEntity article);
+
+    Task<BillEntity> SaveAsync(IEnumerable<BillEntity> article);
+
+    Task<BillEntity> PatchAsync(BillEntity article);
+}
---
+++ Project.Business/Interface/IBillDetailsBusiness.cs
@@ -0,0 +1,28 @@
+﻿using Project.Business.Model;
+using Project.DbManagement;
+using SERP.Framework.Common;
+
+namespace Project.Business.Interface;
+
+public interface IBillDetailsBusiness
+{
+    Task<Pagination<BillDetails>> GetAllAsync(BillDetailsQueryModel queryModel);
+
+    Task<IEnumerable<BillDetails>> ListAllAsync(BillDetailsQueryModel queryModel);
+
+    Task<int> GetCountAsync(BillDetailsQueryModel queryModel);
+
+    Task<IEnumerable<BillDetails>> ListByIdsAsync(IEnumerable<Guid> ids);
+
+    Task<BillDetails> FindAsync(Guid contentId);
+
+    Task<BillDetails> DeleteAsync(Guid contentId);
+    
+    Task<IEnumerable<BillDetails>> DeleteAsync(Guid[] deleteIds);
+
+    Task<BillDetails> SaveAsync(BillDetails article);
+
+    Task<BillDetails> SaveAsync(IEnumerable<BillDetails> article);
+
+    Task<BillDetails> PatchAsync(BillDetails article);
+}
