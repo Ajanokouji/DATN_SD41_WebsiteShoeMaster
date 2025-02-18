8022fb6cf986a57c2547676ed057d0ebe15dd797
QuynbDev2k3 <quynbdev@gmail.com>
2025-02-18 19:50:24 +0700

    view_home_admin

---
+++ Project.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.MVC.Areas.Admin.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Home()
+    {
+        return View();
+    }
+}
