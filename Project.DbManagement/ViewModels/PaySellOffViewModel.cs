4716b9635d267036e741cca576c2416456826e23
QuynbDev2k3 <quynbdev@gmail.com>
2025-04-01 16:09:38 +0700

    selloff

---
+++ Project.DbManagement/ViewModels/PaySellOffViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.DbManagement.ViewModels;
+
+public class PaySellOffViewModel
+{
+    public Guid Id { get; set; }
+    public string MaHD { get; set; }
+    public string KhachHang { get; set; }
+    public string NhanVien { get; set; }
+    public DateTime? NgayThanhToan { get; set; }
+    public int TongSL { get; set; }
+    public int? TongTien { get; set; }
+}
---
+++ Project.DbManagement/ViewModels/ProductDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Project.DbManagement.Entity;
+
+namespace Project.DbManagement.ViewModels;
+
+public class ProductDetailsViewModel
+{
+    public Guid Id { get; set; }
+    public string Ten { get; set; }
+    public List<ProductEntity> lstMau { get; set; }
+    public List<ProductEntity> lstKC { get; set; }
+}
