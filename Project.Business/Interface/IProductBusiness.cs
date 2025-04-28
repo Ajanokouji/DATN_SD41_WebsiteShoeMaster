a460705cfcd32ee2e98553ae60909469e189415c
QuynbDev2k3 <quynbdev@gmail.com>
2025-04-28 18:58:48 +0700

    searchproduct_by_name_code

--- Project.Business/Interface/IProductBusiness.cs
+++ Project.Business/Interface/IProductBusiness.cs
@@ -90,5 +90,6 @@ namespace Project.Business.Interface
         public Task<ProductDetailsViewModel> GetProductDetailsById(Guid idprd);
 
         public Task<List<ProductDetailsViewModel>> ListProductDetailsById(Guid idprd);
+        public Task<List<ListProductSellViewModel>> SearchProduct(string keyword);
     }
 }
