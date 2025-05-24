05cbb5b2391e8fdc42521d141df83ad3a48cc8f1
QuynbDev2k3 <quynbdev@gmail.com>
2025-05-24 13:22:19 +0700

    hotfix

--- Project.Business/ServiceCollections.cs
+++ Project.Business/ServiceCollections.cs
@@ -83,11 +83,11 @@ namespace Project.Business
             services.AddScoped<ICartDetailFactory, CartDetailFactory>();
 
 
-            services.AddScoped<IBillIntercepterAfterSave, SendEmailAfterSaveBill>();
+            // services.AddScoped<IBillIntercepterAfterSave, SendEmailAfterSaveBill>();
 
-            var emailSettings = configuration.GetSection("EmailSettings").Get<EmailSettings>();
-
-            services.AddSingleton(emailSettings);
+            // var emailSettings = configuration.GetSection("EmailSettings").Get<EmailSettings>();
+            //
+            // services.AddSingleton(emailSettings);
 
 
             // Configure CORS
